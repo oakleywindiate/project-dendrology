1020ecda232675ddb3bab6eb2e519e7139f3a278
Oakley Windiate <oakley.windiate@gmail.com>
2022-06-09 15:53:16 -0600

    Create paths to connect Components, add async to connect API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import QuestionContainer from './QuestionContainer';
 import './App.css';
 
 function App() {
+  const [questions, setQuestions] = useState([])
+  const [error, setError] = useState('')
+
+  const getQuestions = async () => {
+    const url = 'http://localhost:3001'
+    setError('')
+
+    try {
+      const response = await fetch(url)
+      const loadQuestions = await response.json()
+      setQuestions(loadQuestions)
+    } catch(error) {
+      setError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getQuestions()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>project+dendrology</h1>
       </header>
-      
+      <main>
+        <QuestionContainer questions={questions} />
+      </main>
     </div>
   );
 }
